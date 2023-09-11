0ee7f9a00586051c61b1f37909f21617a9edc82d
Nick James <njames8@outlook.com>
2023-09-11 10:58:58 -0500

    Fixing param misname

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 from sound_generation.tones.base_tones import Tone, Notes
 from sound_generation.sound_generator import play
 
-def default(duration: int):
+def default(duration: float):
     tones = [
         Tone(note=Notes.E, octave=2), 
         Tone(note=Notes.A, octave=2),
@@ -11,7 +11,7 @@ def default(duration: int):
         Tone(note=Notes.B, octave=3),
         Tone(note=Notes.E, octave=4)
     ]
-    play(tones=tones, duration=duration)
+    play(tones=tones, duration_seconds=duration)
 
 def parse_notes(notes_str: str) -> [Tone]:
     notes_list = notes_str.split(',')
@@ -22,19 +22,19 @@ def parse_notes(notes_str: str) -> [Tone]:
         tones.append(Tone(Notes(note), int(octave_str)))
     return tones
 
-def convert_hours_to_seconds(hours: int) -> int:
-   return hours * 60 * 60
+def convert_hours_to_seconds(hours: float) -> int:
+   return int(hours * 60 * 60)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog='Soundboard Stimulator',
         description='Generates sound to break in wooden instruments\' soundboards')
     parser.add_argument('-n', '--notes', dest='notes', type=parse_notes, required=False)
-    parser.add_argument('-d', '--duration', dest='duration', type=int, required=True, help='The length of time in hours to stimulate the soundboard')
+    parser.add_argument('-d', '--duration', dest='duration', type=float, required=True, help='The length of time in hours to stimulate the soundboard')
     args = parser.parse_args()
 
     duration = convert_hours_to_seconds(args.duration)
     if args.notes == None:
         default(duration)
     else:
-        play(tones=args.notes, duration=duration)
+        play(tones=args.notes, duration_seconds=duration)
